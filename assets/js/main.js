3bee060ba0699cd783261721596b303a56e952b4
safa belhoufrte <124907007+safabelhoufrte@users.noreply.github.com>
2024-08-07 16:33:48 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,6 @@
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
 
-  /**
-   * Mobile nav toggle
-   */
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
   function mobileNavToogle() {
@@ -94,12 +91,6 @@
   }
   window.addEventListener('load', aosInit);
 
-  /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
 
   /**
    * Animate the skills items on reveal
@@ -117,6 +108,7 @@
       }
     });
   });
+
 
   /**
    * Init swiper sliders
@@ -218,5 +210,39 @@
   }
   window.addEventListener('load', navmenuScrollspy);
   document.addEventListener('scroll', navmenuScrollspy);
+
+
+      // start-carousel
+      $(".start-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 20000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
 
 })();
